568e65c60adc5a9cfb0ec38593bf7fbe9e336b59
Runwei Qiang <qiangrw@gmail.com>
2013-04-05 21:25:32 +0800

    minor change

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,84 +1,85 @@
 <div id="slider">
-		<div class="viewer">
-			<div class="reel">
-				<div class="slide">
-					<img src="images/slide01.jpg" alt="" />
-				</div>
-				<div class="slide">
-					<img src="images/slide02.jpg" alt="" />
-				</div>
-				<div class="slide">
-					<img src="images/slide03.jpg" alt="" />
-				</div>
-				<div class="slide">
-					<img src="images/slide04.jpg" alt="" />
-				</div>
-				<div class="slide">
-					<img src="images/slide05.jpg" alt="" />
-				</div>
-			</div>
-		</div>
-		<div class="indicator">
-			<span>1</span>
-			<span>2</span>
-			<span>3</span>
-			<span>4</span>
-			<span>5</span>
-		</div>
-  </div>
+    <div class="viewer">
+        <div class="reel">
+            <div class="slide">
+                <img src="images/slide01.jpg" alt="" />
+            </div>
+            <div class="slide">
+                <img src="images/slide02.jpg" alt="" />
+            </div>
+            <div class="slide">
+                <img src="images/slide03.jpg" alt="" />
+            </div>
+            <div class="slide">
+                <img src="images/slide04.jpg" alt="" />
+            </div>
+            <div class="slide">
+                <img src="images/slide05.jpg" alt="" />
+            </div>
+        </div>
+    </div>
+    <div class="indicator">
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span>4</span>
+        <span>5</span>
+    </div>
+</div>
 
-	<div id="page">
-        <div id="content">
-           <div class="box">
-                <h3>EduRunner彩虹行动成长夏令营志愿者招募</h3>
-                <p>
-                抬头仰望星空，低头汗水挥洒大地。每个在路上的人，心里都有着最深沉的爱，从不需繁琐的言语，因为一直在用行动来证明对于这块大地的执着。在这如水的年华里，我想与这样一群人分享我的爱，我的泪和我的汗。
-                </p>
-                <p>
-这个夏天，EduRunner彩虹行动成长夏令营，来到北京蒲公英中学、浙江三门县，给这里的孩子带来个性发展、公民课程、和公益社会实践课程的成长夏令营。（<a href=<?php echo site_url('support/volunteer') ?>>阅读更多</a>）
-                <p>
-			</div>
-  
-            <div class="box">
-				<h3>EduRunner</h3>
-				<p>教育先行（EduRunner）成立于2011年，是一个由留美学子创立的、在美国注册的非营利组织。我们关注教育公平，致力于青少年个人发展的咨询和培训。通过提供与能力建设相关的培训课程、工作坊等活动，促进青少年个性发展，提高他们团队合作和社会参与的能力。
-				</p>
-				<p>目前，教育先行的主打活动是“彩虹行动”新市民子弟多彩夏令营。</p>
-			</div>
-			<div class="box">
-				<h3>彩虹行动</h3>
-				<ul class="section-list">
-					<li class="first">
-						<span> 彩虹行动是“教育先行”的主打活动, 其目标对象是城市中的新市民（外来务工者）的子女。“彩虹行动”夏令营为期四天，从认识自我、认识他人、认识世界三个目标出发，围绕人生规划、时间管理、团队合作、批判性思维等主题开展课程和活动，通过互动课堂、团队合作游戏和实践社区服务项目等形式提升新市民子弟的自信心和创造力、增强自我意识、增进团队合作精神。</span>
-					</li>
-					<li class="last">
-						<!--<img class="pic alignleft" src="images/pic03.jpg" width="70" height="70" alt="" />-->
-					</li>
-				</ul>
-			</div>
-			
-			<br class="clearfix" />
-		</div>
-		<div id="sidebar">
-			<div class="box">
-				<h3>最新消息</h3>
-				<ul class="list">
+<div id="page">
+    <div id="content">
+        <div class="box">
+            <h3>EduRunner彩虹行动成长夏令营志愿者招募</h3>
+            <p>
+            抬头仰望星空，低头汗水挥洒大地。每个在路上的人，心里都有着最深沉的爱，从不需繁琐的言语，因为一直在用行动来证明对于这块大地的执着。在这如水的年华里，我想与这样一群人分享我的爱，我的泪和我的汗。
+            </p>
+            <p>
+            这个夏天，EduRunner彩虹行动成长夏令营，来到北京蒲公英中学、浙江三门县，给这里的孩子带来个性发展、公民课程、和公益社会实践课程的成长夏令营。（<a href=<?php echo site_url('support/volunteer') ?>>阅读更多</a>）
+            <p>
+        </div>
+
+        <div class="box">
+            <h3>EduRunner</h3>
+            <p>
+            教育先行（EduRunner）成立于2011年，是一个由留美学子创立的、在美国注册的公益组织。我们关注教育公平，致力于青少年个人发展的咨询和培训。通过提供与能力建设相关的培训课程、工作坊等活动，促进青少年个性发展，提高他们团队合作和社会参与的能力。                
+            </p>
+            <p>目前，教育先行的主打活动是“彩虹行动”新市民子弟多彩夏令营。</p>
+        </div>
+        <div class="box">
+            <h3>彩虹行动</h3>
+            <ul class="section-list">
+                <li class="first">
+                <span> 彩虹行动是“教育先行”的主打活动, 其目标对象是城市中的新市民（外来务工者）的子女。“彩虹行动”夏令营为期四天，从认识自我、认识他人、认识世界三个目标出发，围绕人生规划、时间管理、团队合作、批判性思维等主题开展课程和活动，通过互动课堂、团队合作游戏和实践社区服务项目等形式提升新市民子弟的自信心和创造力、增强自我意识、增进团队合作精神。</span>
+                </li>
+                <li class="last">
+                <!--<img class="pic alignleft" src="images/pic03.jpg" width="70" height="70" alt="" />-->
+                </li>
+            </ul>
+        </div>
+
+        <br class="clearfix" />
+    </div>
+    <div id="sidebar">
+        <div class="box">
+            <h3>最新消息</h3>
+            <ul class="list">
                 <li class="first"><a href="<?php echo site_url('support/volunteer'); ?>">2013年彩虹行动暑期夏令营志愿者招募已经开始</a></li>
                 <li><a href="<?php echo site_url('welcome') ?>">EduRunner官方网站正式上线，欢迎关注！</a></li>
-					<!--<li class="last"><a href="#">更多消息</a></li>-->
-				</ul>
-			</div>
-			<div class="box">
-				<h3>彩虹新闻</h3>
-				<div class="date-list">
-					<ul class="list date-list">
-						<li><span class="date">08/08/05</span><br />
-						<a href="#">外来工子弟体验哈佛教育</a></li>
-						<li> <a href="#">更多新闻</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-		<br class="clearfix" />
-	</div>
+                <!--<li class="last"><a href="#">更多消息</a></li>-->
+            </ul>
+        </div>
+        <div class="box">
+            <h3>彩虹新闻</h3>
+            <div class="date-list">
+                <ul class="list date-list">
+                    <li><span class="date">08/08/05</span><br />
+                    <a href="#">外来工子弟体验哈佛教育</a></li>
+                    <li> <a href="#">更多新闻</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <br class="clearfix" />
+</div>
 </div>
