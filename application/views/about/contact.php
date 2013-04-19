71a7410568c419459586aaed257f736ae27a6bae
Runwei Qiang <qiangrw@gmail.com>
2013-04-19 08:49:23 +0800

    add analysis tool

--- application/views/about/contact.php
+++ application/views/about/contact.php
@@ -2,7 +2,7 @@
 <h2>联系方式</h2>
 
 <ul>
-    <li>网址: <a href="www.edurunner.org">www.edurunner.org</a></li>
+    <li>网址: <a href="http://www.edurunner.org">www.edurunner.org</a></li>
     <li>人人：EduRunner教育先行</li>
 	<li>微博：EduRunner教育先行</li>
     <li>邮箱: edurunner@163.com</li>
--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -1,5 +1,9 @@
 <div id="footer">
 	Copyright (c) 2012 edurunner.org. All rights reserved.
 </div>
+
+<div style="display:none!important;" id="clustrmaps-widget"></div><script type="text/javascript">var _clustrmaps = {'url' : 'http://www.edurunner.org', 'user' : 1089684, 'server' : '2', 'id' : 'clustrmaps-widget', 'version' : 1, 'date' : '2013-04-15', 'lang' : 'en', 'corners' : 'square' };(function (){ var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'http://www2.clustrmaps.com/counter/map.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);})();</script><noscript><a href="http://www2.clustrmaps.com/user/a9110a094"><img src="http://www2.clustrmaps.com/stats/maps-no_clusters/www.edurunner.org-thumb.jpg" alt="Locations of visitors to this page" /></a></noscript>
+
+
 </body>
 </html>
