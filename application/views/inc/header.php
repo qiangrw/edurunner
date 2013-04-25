46e0deb257d377bdc15f530aa8ce15a44c9601c9
Runwei Qiang <qiangrw@gmail.com>
2013-04-25 09:54:49 +0800

    minor change

--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -103,7 +103,9 @@ $(function() {
 
         <!-- THIRD -->
         <li><span class="opener">新闻与资源<b></b></span>
-            <ul><li><a href="<?php echo site_url('resource/news'); ?>">彩虹行动相关新闻</a></li>
+            <ul>
+                <li><a href="http://www.edurunner.org/blog">彩虹博客</a></li>
+                <li><a href="<?php echo site_url('resource/news'); ?>">彩虹行动相关新闻</a></li>
                 <li><a href="<?php echo site_url('resource'); ?>">彩虹行动宣传材料</a></li>
                 <li><a href="<?php echo site_url('resource/posts'); ?>">志愿者感言</a></li>
             </ul>
--- application/views/inc/post_header.php
+++ application/views/inc/post_header.php
@@ -80,7 +80,9 @@ Edit by Runwei Qiang
 
         <!-- THIRD -->
         <li><span class="opener">新闻与资源<b></b></span>
-            <ul><li><a href="<?php echo site_url('resource/news'); ?>">彩虹行动相关新闻</a></li>
+            <ul><li>
+                <li><a href="http://www.edurunner.org/blog">彩虹博客</a></li>
+                <a href="<?php echo site_url('resource/news'); ?>">彩虹行动相关新闻</a></li>
                 <li><a href="<?php echo site_url('resource'); ?>">彩虹行动宣传材料</a></li>
                 <li><a href="<?php echo site_url('resource/posts'); ?>">志愿者感言</a></li>
             </ul>
